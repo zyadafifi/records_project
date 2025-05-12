ecf79627dcbe99201376449fb198d9e04d4b0989
zyad afifi <zyad90879@gmail.com>
2025-05-12 13:08:58 +0300

    Add records.js

--- records.js
+++ records.js
@@ -936,7 +936,7 @@ function calculatePronunciationScore(transcript, expectedSentence) {
   recognizedTextDiv.innerHTML = `
     <div style="margin-bottom: 10px;">
       <strong>Original:</strong><br>
-      <p class="sentence-text">Sentence will appear here</p>
+      <p class="sentence-text">${originalSentenceText}</p>
     </div>
     <div>
       <strong>You said:</strong><br>
